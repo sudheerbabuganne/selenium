6d961ab017114c2212be8492befe4ba0c153420c
pragna <sudheerbabuganne@gmail.com>
2018-02-11 19:20:34 +0530

    third commit

--- selenium/src/test/java/examples/FirstTest.java
+++ selenium/src/test/java/examples/FirstTest.java
@@ -1,49 +1,12 @@
 package examples;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.*;
-
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class FirstTest 
 {
 	
-	WebDriver driver=null;
-	
-	@BeforeTest
-	
-	public void initialize()
+	public static void main(String[] args) throws Exception
 	{
-		
-		driver=new FirefoxDriver();
-		
-		driver.manage().window().maximize();
-		
-	}
-	
-	@Test
-	
-	public void launch() throws InterruptedException
-	{
-		
-		driver.get("http://localhost/login.html");
-		
-		Thread.sleep(100);
-		
-		String title=driver.getTitle();
-		
-		System.out.println(title);
-		
+		System.out.println("THird checkout");
 	}
 	
 	
-	@AfterTest
-	
-	public void tearDown()
-	{
-		
-		driver.close();
-	}
-
 }
